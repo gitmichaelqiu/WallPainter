bff94891e6f0308e29c90f3e145a2cbc3a58ae0f
Michael Y. Qiu <altmickymike@outlook.com>
2026-08-30 19:59:03 +0800

    chore: add WallPainter Xcode project

---
+++ WallPainter/WallPainterApp.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+
+@main
+struct WallPainterApp: App {
+    var body: some Scene {
+        WindowGroup("WallPainter") {
+            SettingsView()
+        }
+        .defaultSize(width: 750, height: 550)
+        .windowResizability(.contentMinSize)
+        .windowToolbarStyle(.unifiedCompact)
+
+        Settings {
+            SettingsView()
+        }
+        .defaultSize(width: 750, height: 550)
+        .windowResizability(.contentMinSize)
+    }
+}
