ba9739c0567a26a4791897c20263b50cf9e290d9
Michael Y. Qiu <altmickymike@outlook.com>
2026-08-31 11:06:19 +0800

    fix: apply settings window minimum after content setup

--- WallPainter/Views/StatusBarController.swift
+++ WallPainter/Views/StatusBarController.swift
@@ -158,7 +158,6 @@ final class StatusBarController: NSObject, NSMenuDelegate, NSWindowDelegate {
         window.titlebarSeparatorStyle = .none
         window.isMovableByWindowBackground = true
         window.center()
-        window.minSize = NSSize(width: 750, height: 550)
         window.collectionBehavior = [.participatesInCycle, .fullScreenNone]
         window.standardWindowButton(.zoomButton)?.isEnabled = false
         window.level = .normal
@@ -170,6 +169,7 @@ final class StatusBarController: NSObject, NSMenuDelegate, NSWindowDelegate {
             initialTab: tab
         )
         window.contentViewController = settingsViewController
+        window.minSize = NSSize(width: 750, height: 550)
         window.delegate = self
 
         let windowController = NSWindowController(window: window)
