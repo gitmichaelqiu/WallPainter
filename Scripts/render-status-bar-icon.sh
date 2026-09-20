027d8366b3f404a7ccfccf140b42d47008d8dc45
Michael Y. Qiu <altmickymike@outlook.com>
2026-09-20 22:52:55 +0800

    fix: align status bar icon stroke weight

--- Scripts/render-status-bar-icon.sh
+++ Scripts/render-status-bar-icon.sh
@@ -19,7 +19,7 @@ render_outline() {
         -alpha extract \
         -resize "${size}!" \
         -threshold 50% \
-        -morphology EdgeOut Disk:40 \
+        -morphology EdgeOut Disk:52 \
         -morphology Close Disk:6 \
         "$edge"
 
@@ -53,6 +53,7 @@ back_panel="$(render_outline "$asset_root/4_shape1.png" 896x504 back-panel)"
 front_panel="$(render_outline "$asset_root/6_shape2_rounded copy.png" 768x432 front-panel)"
 wallpainter_mark="$(render_silhouette "$asset_root/ios-appearance-icon-transparent.png" 410x410 wallpainter-mark)"
 composited="$work_dir/composited.png"
+scaled="$work_dir/scaled.png"
 alpha="$work_dir/final-alpha.png"
 
 magick -size 1024x1024 xc:none \
@@ -63,6 +64,15 @@ magick -size 1024x1024 xc:none \
     "$composited"
 
 magick "$composited" \
+    -trim \
+    +repage \
+    -resize 34x34 \
+    -background none \
+    -gravity center \
+    -extent 36x36 \
+    "$scaled"
+
+magick "$scaled" \
     -alpha extract \
     -threshold 28% \
     "$alpha"
